b270151f4748d15a627d4f2797e6363429e4db61
maazh10 <maaz.hashmi@mail.utoronto.ca>
2021-08-07 05:13:59 -0500

    Update fast_fingers.py

--- fast_fingers.py
+++ fast_fingers.py
@@ -2,44 +2,51 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("https://10fastfingers.com/typing-test/english")
-sleep(2)
-driver.find_element_by_xpath(
-    '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
-sleep(5)
-driver.find_element_by_xpath(
-    '//*[@id="fs-slot-footer-wrapper"]/button').click()
+class TypingBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://10fastfingers.com/typing-test/english")
 
-def get_word_list():
-    wlist = driver.find_element_by_id("wordlist").get_attribute("innerHTML")
-    return wlist.split('|')
+    def initialize(self):
+        sleep(2)
+        self.driver.find_element_by_xpath(
+            '//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]').click()
+        sleep(5)
+        self.driver.find_element_by_xpath(
+            '//*[@id="fs-slot-footer-wrapper"]/button').click()
 
-def type_words(box, wpm, w_cooldown=0, l_cooldown=0):
-    timer = driver.find_element_by_id("timer")
-    if l_cooldown > 0:
-        i = 0
-        while i < wpm and timer.text != "0:00":
-            print('typing "' + wlist[i] + '"...')
-            for key in wlist[i]:
-                box.send_keys(key)
-                sleep(l_cooldown)
-            box.send_keys(Keys.SPACE)
-            i += 1
-            sleep(w_cooldown)
-    else:
-        i = 0
-        while i < wpm and timer.text != "0:00":
-            print('typing "' + wlist[i] + '"...')
-            box.send_keys(wlist[i])
-            box.send_keys(Keys.SPACE)
-            i += 1
-            sleep(w_cooldown)
+    def get_word_list(self):
+        wlist = self.driver.find_element_by_id("wordlist").get_attribute("innerHTML")
+        return wlist.split('|')
 
-input_box = driver.find_element_by_xpath('//*[@id="inputfield"]')
-input_box.click()
-sleep(10)
-wlist = get_word_list()
-type_words(input_box, len(wlist))
+    def type_words(self, wpm, w_cooldown=0, l_cooldown=0):
+        box = self.driver.find_element_by_xpath('//*[@id="inputfield"]')
+        box.click()
+        wlist = self.get_word_list()
+        timer = self.driver.find_element_by_id("timer")
+        if l_cooldown > 0:
+            i = 0
+            while i < wpm and timer.text != "0:00":
+                print('typing "' + wlist[i] + '"...')
+                for key in wlist[i]:
+                    box.send_keys(key)
+                    sleep(l_cooldown)
+                box.send_keys(Keys.SPACE)
+                i += 1
+                sleep(w_cooldown)
+        else:
+            i = 0
+            while i < wpm and timer.text != "0:00":
+                print('typing "' + wlist[i] + '"...')
+                box.send_keys(wlist[i])
+                box.send_keys(Keys.SPACE)
+                i += 1
+                sleep(w_cooldown)
+
+bot = TypingBot()
+bot.initialize()
+wlist = bot.get_word_list()
+input("Press any key...")
+bot.type_words(len(wlist))
 
 print("end")
